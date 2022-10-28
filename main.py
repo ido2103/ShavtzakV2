6a43e92920474bfc7a30d2428889787d3c8b5f6a
ido2103 <44828424+ido2103@users.noreply.github.com>
2022-10-29 01:30:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from funcs import append_json
 from GUI import Window
 import sys
-import json
+from funcs import seperate_to_divisions
 from PyQt5.QtWidgets import *
 
-#append_json("Nir",0,0,0,0,0,0,0, True, True, False, False)
 
 def app():
     app = QApplication(sys.argv)
     screen = Window()
     screen.show()
     sys.exit(app.exec_())
+
 
 app()
