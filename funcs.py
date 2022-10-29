75493bd5ea88aae4bea48efd69ef9cabe747e947
ido2103 <44828424+ido2103@users.noreply.github.com>
2022-10-29 15:23:07 +0300

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 
 def append_json(name: str, sg, tapuz, hamal, siur, mitbah, restingHours, mitbahCD, isHamal, isPtorMitbah, isPtorShmira, isSevevMp, division):
@@ -24,10 +25,60 @@ def append_json(name: str, sg, tapuz, hamal, siur, mitbah, restingHours, mitbahC
 
 
 def seperate_to_divisions(json):
-    # returns a list with everyone from all of the divisions
+    # returns a list with everyone from all the divisions
     divs = [[],[],[],[],[]]
     for d in range(len(json)):
         x = (json[d]["Name:"],json[d]["Division"])
         divs[int(x[1])-1].append(x)
     return divs
 
+
+def highest(data, string):
+    high = sorted(data, key=lambda d: d[string], reverse=True)
+    return high[0][string]
+
+
+def doMitbah(data):
+    # TODO: UPDATE THE DATA
+    # selects 2 soldiers to go to the kitchen.
+    chosen = []
+    options = []
+    # get the soldier with the highest mitbah
+    highestMit = highest(data, "Mitbah:")
+    # adding only people who can go to the kitchen
+    for i in data:
+        if (i["Resting Hours:"] <= 0) and (i["Mitbah Cooldown:"] <= 0) and (i["Mitbah:"] <= highestMit)\
+                and (i["IsPtorMitbah"] == "False"):
+            # this section makes it linear. the more difference there is between the current soldier and the soldier
+            # with the highest mitbah value the more likely it is to pick them.
+            for n in range(i["Mitbah:"], highestMit):
+                options.append(i)
+    # select 2 random soldiers out of the list
+    for i in range(2):
+        a = random.choice(options)
+        chosen.append(a["Name:"])
+        for a in options:
+            options.remove(a)
+    return chosen, data
+
+
+def doShmira(data, type):
+    options = []
+    highestShmira = highest(data, type)
+    for i in data:
+        if (i["Resting Hours:"] <= 0) and (i[type] <= highestShmira):
+            # this section makes it linear. the more difference there is between the current soldier and the soldier
+            # with the highest mitbah value the more likely it is to pick them.
+            for n in range(i[type], highestShmira):
+                options.append(i)
+    return random.choice(options)["Name:"]
+
+
+def doHamal(data):
+    options = []
+    highestHamal = highest(data, "Hamal:")
+    for i in data:
+        if (i["Resting Hours:"] <= 0) and (i["Hamal:"] <= highestHamal) and (i["IsHamal"] == "True"):
+            for n in range(i["Hamal:"], highestHamal):
+                options.append(i)
+    return random.choice(options)["Name:"]
