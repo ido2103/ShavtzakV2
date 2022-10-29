453e7d5a2faa231d811c3b3d1064231f579ebe71
ido2103 <44828424+ido2103@users.noreply.github.com>
2022-10-29 15:22:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from GUI import Window
 import sys
-from funcs import seperate_to_divisions
+import json
+from funcs import doMitbah, doShmira, doHamal
 from PyQt5.QtWidgets import *
 
 
@@ -11,4 +12,19 @@ def app():
     sys.exit(app.exec_())
 
 
+with open("soldiers.json", "r") as f:
+        data = json.load(f)
+        for i in data:
+            for n in i:
+                try:
+                    i[n] = int(i[n])
+                except ValueError as exc:
+                    pass
+
+
+print(doMitbah(data)[0])
+print(doShmira(data, "Tapuz:"))
+print(doShmira(data, "S.G:"))
+print(doHamal(data))
+# TODO ADD SIUR
 app()
