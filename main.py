b029c672dd4a3fb8e81998684623d6fd98e72d9f
ido2103 <44828424+ido2103@users.noreply.github.com>
2022-10-31 11:12:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import pstats
-
 from GUI import Window
 import sys
 import json
-from funcs import doMitbah, doShmira, doHamal, doSiur, printAllFuncs
+from funcs import computeList
 from PyQt5.QtWidgets import *
 import cProfile
 
@@ -23,13 +22,5 @@ with open("soldiers.json", "r") as f:
                     i[n] = int(i[n])
                 except ValueError as exc:
                     pass
-
-with cProfile.Profile() as pr:
-    for i in range(10000):
-        print(cycle(data, 2, 2, debug=True))
-
-stats = pstats.Stats(pr)
-stats.sort_stats(pstats.SortKey.TIME)
-stats.print_stats()
-
+computeList(data, 2, 3, debug=False)
 app()
