30ac749e03e51d0fae20d3b3762bd1ede11b173f
ido2103 <44828424+ido2103@users.noreply.github.com>
2022-11-09 19:16:45 +0200

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -78,40 +78,50 @@ def doHamal(data):
     return random.choice(options), data
 
 
-def doSiur(data, amountOfSoldiers):
+def doSiur(data, amountOfSoldiers, sevev):
     # this function is the most complicated one since siurim have to be from the same division. first we get the
     # highest number of siurim from every division
     divisions = seperate_to_divisions(data)
     div1, div2, div3, div4 = [], [], [], []
-    highest1 = highest(divisions[0], "Siur:")
-    highest2 = highest(divisions[1], "Siur:")
-    highest3 = highest(divisions[2], "Siur:")
-    highest4 = highest(divisions[3], "Siur:")
+    if sevev == "מפ":
+        highest1 = highest(divisions[0], "Kaf Kaf A:")
+        highest2 = highest(divisions[1], "Kaf Kaf A:")
+    elif sevev == "סמפ":
+        highest3 = highest(divisions[2], "Kaf Kaf A:")
+        highest4 = highest(divisions[3], "Kaf Kaf A:")
+    else:
+        highest1 = highest(divisions[0], "Kaf Kaf A:")
+        highest2 = highest(divisions[1], "Kaf Kaf A:")
+        highest3 = highest(divisions[2], "Kaf Kaf A:")
+        highest4 = highest(divisions[3], "Kaf Kaf A:")
     # separating everyone into their divisions
     for i in data:
-        match i["Division"]:
-            case 1:
-                if (i["Siur:"] <= highest1) and (i["Resting Hours:"] <= 0):
-                    for n in range(i["Siur:"], highest1+1):
-                        div1.append(i)
-                pass
-            case 2:
-                if (i["Siur:"] <= highest2) and (i["Resting Hours:"] <= 0):
-                    for n in range(i["Siur:"], highest2+1):
-                        div2.append(i)
-                pass
-            case 3:
-                if (i["Siur:"] <= highest3) and (i["Resting Hours:"] <= 0):
-                    for n in range(i["Siur:"], highest3+1):
-                        div3.append(i)
-                pass
-            case 4:
-                if (i["Siur:"] <= highest4) and (i["Resting Hours:"] <= 0):
-                    for n in range(i["Siur:"], highest4+1):
-                        div4.append(i)
-                pass
-            case _:
-                pass
+        try:
+            match i["Division"]:
+                case 1:
+                    if (i["Siur:"] <= highest1) and (i["Resting Hours:"] <= 0):
+                        for n in range(i["Siur:"], highest1+1):
+                            div1.append(i)
+                    pass
+                case 2:
+                    if (i["Siur:"] <= highest2) and (i["Resting Hours:"] <= 0):
+                        for n in range(i["Siur:"], highest2+1):
+                            div2.append(i)
+                    pass
+                case 3:
+                    if (i["Siur:"] <= highest3) and (i["Resting Hours:"] <= 0):
+                        for n in range(i["Siur:"], highest3+1):
+                            div3.append(i)
+                    pass
+                case 4:
+                    if (i["Siur:"] <= highest4) and (i["Resting Hours:"] <= 0):
+                        for n in range(i["Siur:"], highest4+1):
+                            div4.append(i)
+                    pass
+                case _:
+                    pass
+        except UnboundLocalError:
+            pass
     match amountOfSoldiers:
         # setting up the soldiers to the same amount it was set to
         case 1:
@@ -120,11 +130,11 @@ def doSiur(data, amountOfSoldiers):
             if div3: div3 = random.choice(div3)
             if div4: div4 = random.choice(div4)
 
-        case 2: # ADD: IF THEY LITERALLY DON'T HAVE ENOUGH PEOPLE FOR A SIUR THE WHILE LOOP'S STUCK INDEFINETLY.
+        case 2:
             if div1:
                 temp = [random.choice(div1)]
                 count = 0
-                while len(temp) < amountOfSoldiers and count < 10:
+                while len(temp) < amountOfSoldiers and count < 20:
                     a = random.choice(div1)
                     if a not in temp: temp.append(a)
                     count += 1
@@ -133,7 +143,7 @@ def doSiur(data, amountOfSoldiers):
             if div2:
                 temp = [random.choice(div2)]
                 count = 0
-                while len(temp) < amountOfSoldiers and count < 10:
+                while len(temp) < amountOfSoldiers and count < 20:
                     a = random.choice(div2)
                     if a not in temp: temp.append(a)
                     count += 1
@@ -141,7 +151,7 @@ def doSiur(data, amountOfSoldiers):
             if div3:
                 temp = [random.choice(div3)]
                 count = 0
-                while len(temp) < amountOfSoldiers and count < 10:
+                while len(temp) < amountOfSoldiers and count < 20:
                     a = random.choice(div3)
                     if a not in temp: temp.append(a)
                     count += 1
@@ -149,18 +159,22 @@ def doSiur(data, amountOfSoldiers):
             if div4:
                 temp = [random.choice(div4)]
                 count = 0
-                while len(temp) < amountOfSoldiers:
-                    a = random.choice(div3)
+                while len(temp) < amountOfSoldiers and count < 20:
+                    a = random.choice(div4)
                     if a not in temp: temp.append(a)
                     count += 1
                 div4 = temp
         case _:
             raise ValueError
     soldiers = []
-    soldiers.append(div1)
-    soldiers.append(div2)
-    soldiers.append(div3)
-    soldiers.append(div4)
+    if div1:
+        soldiers += [div1]
+    if div2:
+        soldiers += [div2]
+    if div3:
+        soldiers += [div3]
+    if div4:
+        soldiers += [div4]
     for i in soldiers:
         if not i:
             soldiers.remove(i)
@@ -170,35 +184,45 @@ def doSiur(data, amountOfSoldiers):
     return a, data
 
 
-def KafKafA(data, amountOfKKA):
+def KafKafA(data, amountOfKKA, sevev):
     divisions = seperate_to_divisions(data)
     div1, div2, div3, div4 = [], [], [], []
-    highest1 = highest(divisions[0], "Kaf Kaf A:")
-    highest2 = highest(divisions[1],"Kaf Kaf A:")
-    highest3 = highest(divisions[2], "Kaf Kaf A:")
-    highest4 = highest(divisions[3], "Kaf Kaf A:")
+    if sevev == "מפ":
+        highest1 = highest(divisions[0], "Kaf Kaf A:")
+        highest2 = highest(divisions[1],"Kaf Kaf A:")
+    elif sevev == "סמפ":
+        highest3 = highest(divisions[2], "Kaf Kaf A:")
+        highest4 = highest(divisions[3], "Kaf Kaf A:")
+    else:
+        highest1 = highest(divisions[0], "Kaf Kaf A:")
+        highest2 = highest(divisions[1], "Kaf Kaf A:")
+        highest3 = highest(divisions[2], "Kaf Kaf A:")
+        highest4 = highest(divisions[3], "Kaf Kaf A:")
     if amountOfKKA == 1:
         return [random.choice(data)], data
     for i in data:
-        match i["Division"]:
-            case 1:
-                if i["Kaf Kaf A:"] <= highest1 and i["Resting Hours:"] <= 0:
-                    for j in range(i["Kaf Kaf A:"], highest1+1):
-                        div1.append(i)
-            case 2:
-                if i["Kaf Kaf A:"] <= highest2 and i["Resting Hours:"] <= 0:
-                    for j in range(i["Kaf Kaf A:"], highest2 + 1):
-                        div2.append(i)
-            case 3:
-                if i["Kaf Kaf A:"] <= highest3 and i["Resting Hours:"] <= 0:
-                    for j in range(i["Kaf Kaf A:"], highest3 + 1):
-                        div3.append(i)
-            case 4:
-                if i["Kaf Kaf A:"] <= highest4 and i["Resting Hours:"] <= 0:
-                    for j in range(i["Kaf Kaf A:"], highest4 + 1):
-                        div4.append(i)
-            case _:
-                pass
+        try:
+            match i["Division"]:
+                case 1:
+                    if i["Kaf Kaf A:"] <= highest1 and i["Resting Hours:"] <= 0:
+                        for j in range(i["Kaf Kaf A:"], highest1+1):
+                            div1.append(i)
+                case 2:
+                    if i["Kaf Kaf A:"] <= highest2 and i["Resting Hours:"] <= 0:
+                        for j in range(i["Kaf Kaf A:"], highest2 + 1):
+                            div2.append(i)
+                case 3:
+                    if i["Kaf Kaf A:"] <= highest3 and i["Resting Hours:"] <= 0:
+                        for j in range(i["Kaf Kaf A:"], highest3 + 1):
+                            div3.append(i)
+                case 4:
+                    if i["Kaf Kaf A:"] <= highest4 and i["Resting Hours:"] <= 0:
+                        for j in range(i["Kaf Kaf A:"], highest4 + 1):
+                            div4.append(i)
+                case _:
+                    pass
+        except UnboundLocalError:
+            pass
     if div1:
         temp = [random.choice(div1)]
         count = 0
@@ -231,7 +255,15 @@ def KafKafA(data, amountOfKKA):
             if a not in temp: temp.append(a)
             count += 1
         div4 = temp
-    soldiers = [div1, div2, div3, div4]
+    soldiers = []
+    if div1:
+        soldiers += [div1]
+    if div2:
+        soldiers += [div2]
+    if div3:
+        soldiers += [div3]
+    if div4:
+        soldiers += [div4]
     r = random.choice(soldiers)
     return r, data
 
@@ -263,7 +295,7 @@ def return_score(data, soldiers, amount_of_siurim, amount_of_kka):
         else: pass
     return score
 
-def cycle(data, amountOfSoldiers, amountOfSiurim, amountOfKKA, debug, num):
+def cycle(data, amountOfSoldiers, amountOfSiurim, amountOfKKA, debug, num, sevev):
     soldiers = []
     # First thing's first, doing the mitbah and giving them a 24 hour break from doing missions.
     if num == 1:
@@ -277,7 +309,7 @@ def cycle(data, amountOfSoldiers, amountOfSiurim, amountOfKKA, debug, num):
                 i["Mitbah Cooldown:"] = 2
                 i["Resting Hours:"] = 24
             soldiers.append((i, "Mitbah"))
-        kafkafa, data = KafKafA(data, amountOfKKA)
+        kafkafa, data = KafKafA(data, amountOfKKA, sevev)
         for i in kafkafa:
             for j in data:
                 if not debug:
@@ -294,7 +326,7 @@ def cycle(data, amountOfSoldiers, amountOfSiurim, amountOfKKA, debug, num):
         soldiers.append((hamal, "Hamal"))
     # then, siur.
     if num == 1 or (num == 3 and amountOfSiurim > 1) or (num == 5 and amountOfSiurim > 2):
-        siur, data = doSiur(data, amountOfSoldiers)
+        siur, data = doSiur(data, amountOfSoldiers, sevev)
         for i in siur:
             if not debug:
                 i["Siur:"] += 1
@@ -318,24 +350,60 @@ def cycle(data, amountOfSoldiers, amountOfSiurim, amountOfKKA, debug, num):
     return soldiers, data
 
 
-def computeList(data, amountOfSoldiers, amountOfSiurim, amountOfKKA, attempts, debug):
+def json_to_iter(sevev, inactive):
+    with open("soldiers.json", "r") as f:
+        data = json.load(f)
+        for i in data:
+            for n in i:
+                try:
+                    i[n] = int(i[n])
+                except ValueError as exc:
+                    pass
+    for i in inactive:
+        data.remove(i)
+
+    if sevev == "מפ":
+        for j in range(5):
+            for i in data:
+                if i["Sevev"] == "SMP":
+                    data.remove(i)
+
+    if sevev ==  "סמפ":
+        for i in data:
+            if i["Sevev"] == "MP":
+                data.remove(i)
+                print(i)
+
+    with open("temp_data.json", "w") as f:
+        json.dump(data, f)
+
+
+def computeList(amountOfSoldiers, amountOfSiurim, amountOfKKA, attempts, sevev, debug):
     best_score = -999
     for j in range(attempts):
-        data_to_iter = copy.deepcopy(data)
+        with open("soldiers.json", "r") as f:
+            data_to_iter = json.load(f)
+        for i in data_to_iter:
+            for n in i:
+                try:
+                    i[n] = int(i[n])
+                except ValueError as exc:
+                    pass
         temp_score = -99
         soldiers = []
         for i in range(1, 7):
-            temp, data_to_iter = (cycle(data_to_iter, amountOfSoldiers, amountOfSiurim, amountOfKKA, debug, i))
+            temp, data_to_iter = (cycle(data_to_iter, amountOfSoldiers, amountOfSiurim, amountOfKKA, debug, i, sevev))
             for key in temp:
                 soldiers.append((key[0]["Name:"], key[1]))
         temp_score = return_score(data_to_iter, soldiers, amountOfSoldiers, amountOfKKA)
         if temp_score > best_score:
             best_soldiers = soldiers
             best_score = temp_score
-            best_data = data_to_iter
-        if j % 10 == 0: print(j)
+            best_data = copy.deepcopy(data_to_iter)
+            print(best_data)
+        if j % 100 == 0: print(j)
     with open("soldiers.json", "w") as f:
         f.seek(0)
         json.dump(best_data, f, indent=6)
-
+    print(best_score)
     return best_soldiers
