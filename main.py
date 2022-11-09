b150a222887bfb5e16e3ea564e2cbb136c6b9a00
ido2103 <44828424+ido2103@users.noreply.github.com>
2022-11-09 21:35:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pstats
+
 from GUI import Window
 import sys
 import json
@@ -6,28 +7,18 @@ from funcs import computeList
 from PyQt5.QtWidgets import *
 import cProfile
 
+
 def app():
     app = QApplication(sys.argv)
     screen = Window()
     screen.show()
     sys.exit(app.exec_())
 
-# TODO LIST
-"""
-RETURN SCORE +
-SEVEV MP
-TRANSLATE UI
-GIMELIM
-UPGRADE UI
-KAF KAF A +
-export shavtzak +
-"""
-# TO IMPROVE
-"""
-dont use deepcopy use json instead
-improve score +
-improve kafkafa +
-"""
+
+#computeList(1, 1, 1, 500, "סמפ", False)
+
+
+app()
 
 with open("soldiers.json", "r") as f:
         data = json.load(f)
@@ -38,13 +29,11 @@ with open("soldiers.json", "r") as f:
                 except ValueError as exc:
                     pass
 
-#soldiers = computeList(data, 2, 2, 2, 200, False)
-#print(soldiers)
-"""
-p = cProfile.Profile()
-p.run("computeList(data, 2, 2, 1, 5000, False)")
-s = pstats.Stats(p)
+with cProfile.Profile() as pr:
+        computeList(1, 1, 1, 500, False)
+
+s = pstats.Stats(pr)
 s.sort_stats("cumtime").print_stats()
-#app()
-"""
+s.print_stats()
+
 app()
