5e2936a3a603502886ffe0ffcb48257a76b39d8d
ido2103 <44828424+ido2103@users.noreply.github.com>
2022-12-31 12:37:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from GUI import Window
 import sys
 from funcs import computeList
 from PyQt5.QtWidgets import QApplication
+import pstats
+import cProfile
 
 
 def app():
@@ -9,10 +11,16 @@ def app():
     screen = Window()
     screen.show()
     sys.exit(app.exec_())
-
+"""
+pr = cProfile.Profile()
+pr.enable()
 # mostly used for bug testing without the GUI since the GUI doesn't show exceptions, it just crashes.
-# computeList(1, 1, 3, 1000, "רגיל", [], "", 0)
-
+computeList(1, 1, 3, 1000, "רגיל", [], "", 0)
+pr.disable()
+stats = pstats.Stats(pr)
+stats.sort_stats("tottime")
+stats.print_stats()
+"""
 
 if __name__ == '__main__':
     app()
